9edf1065a3c2e11d060bced41911d7bef8c19d2e
jusish <ishimwejustin67@gmail.com>
2025-05-08 22:02:11 +0200

    [starter]

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { Overview } from "./pages/Overview";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Signup />} path="/signup" />
+        <Route element={<Overview />} path="/overview" />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Users.tsx
@@ -0,0 +1,13 @@
+export const Users = () => {
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>List of users</p>
+      <ul>
+        <li>User 1</li>
+        <li>User 2</li>
+        <li>User 3</li>
+      </ul>
+    </div>
+  );
+};
